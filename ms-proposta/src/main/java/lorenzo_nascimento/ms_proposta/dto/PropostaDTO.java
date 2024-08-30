2329de886f52793895a86f687c85bb79e2506c87
Lorenzo Nascimento <lorenzonascimento.dev@gmail.com>
2024-08-30 12:31:55 -0300

    Atualizando as classes DTO

--- ms-proposta/src/main/java/lorenzo_nascimento/ms_proposta/dto/PropostaDTO.java
+++ ms-proposta/src/main/java/lorenzo_nascimento/ms_proposta/dto/PropostaDTO.java
@@ -1,5 +1,8 @@
 package lorenzo_nascimento.ms_proposta.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +15,17 @@ import java.math.BigDecimal;
 @Setter
 @Getter
 public class PropostaDTO {
+
+    @NotNull(message = "Valor solicitado é obrigatório")
+    @DecimalMin(value = "0.0", inclusive = false, message = "O valor solicitado deve ser maior que zero")
     private BigDecimal valorSolicitado;
+
+    @NotNull(message = "Prazo para pagamento é obrigatório")
+    @Min(value = 1, message = "O prazo para pagamento deve ser no mínimo 1 mês")
     private Integer prazoParaPagamento;
 
+    private boolean aprovado = false; // Campo não obrigatório, pode ser configurado pela lógica do serviço
+
+    @NotNull(message = "ID do usuário é obrigatório")
+    private Long userId; // ID do usuário associado à proposta, se necessário
 }
--- ms-proposta/src/main/java/lorenzo_nascimento/ms_proposta/dto/UserDTO.java
+++ ms-proposta/src/main/java/lorenzo_nascimento/ms_proposta/dto/UserDTO.java
@@ -1,5 +1,8 @@
 package lorenzo_nascimento.ms_proposta.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,10 +15,22 @@ import java.math.BigDecimal;
 @Setter
 @Getter
 public class UserDTO {
-    private String nome;
-    private String sobrenome;
-    private String cpf;
-    private String telefone;
-    private BigDecimal renda;
 
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+
+    @NotBlank(message = "Sobrenome é obrigatório")
+    private String sobrenome;
+
+    @NotBlank(message = "CPF é obrigatório")
+    // Adicione uma validação de formato para CPF, se necessário
+    private String cpf;
+
+    @NotBlank(message = "Telefone é obrigatório")
+    // Adicione uma validação de formato para telefone, se necessário
+    private String telefone;
+
+    @NotNull(message = "Renda é obrigatória")
+    @DecimalMin(value = "0.0", inclusive = false, message = "A renda deve ser maior que zero")
+    private BigDecimal renda;
 }
