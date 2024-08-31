c9ec0038a42d45347a944553c93be0fdc1ffecf7
Lorenzo Nascimento <lorenzonascimento.dev@gmail.com>
2024-08-31 17:11:57 -0300

    Alteração no ConfigModelMapper

--- ms-proposta/src/main/java/lorenzo_nascimento/ms_proposta/config/ConfigModelMapper.java
+++ ms-proposta/src/main/java/lorenzo_nascimento/ms_proposta/config/ConfigModelMapper.java
@@ -1,14 +1,14 @@
-package lorenzo_nascimento.ms_proposta.config;
+package br.com.fiap.ms_pedidos.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration  // Indica que esta é uma classe de configuração do Spring
 public class ConfigModelMapper {
 
-    @Bean
-    public ModelMapper getModelMapper(){
+    @Bean  // Registra o ModelMapper como um bean gerenciado pelo Spring
+    public ModelMapper getModelMapper() {
         return new ModelMapper();
     }
 
